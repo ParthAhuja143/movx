e558f88433816339da4d161146972a0b0359d011
Axadjon <156516832+axadjon18@users.noreply.github.com>
2024-09-08 13:59:03 +0500

    Update Footer.tsx

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 
 const Footer: React.FC = () => (
-  <div className="footer">
-    <span>
-      Made by
-      &nbsp;
-      <a href="https://parthahuja.netlify.com" target="_blank">
-        Parth Ahuja
-      </a>
-      &nbsp;
-      with
-      &nbsp;
-      <i className="fa fa-heart" />
-    </span>
-  </div>
+ <div></div>
 );
 
 export default Footer;
